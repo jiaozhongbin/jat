b84cb48dc8916a4dbad6400d54256be330021dcb
jiaozhongbin <876040320@qq.com>
2016-03-15 17:56:36 +0800

    去掉不用的包

--- src/cn/suning/automation/keyword/BrowserKeyword.java
+++ src/cn/suning/automation/keyword/BrowserKeyword.java
@@ -15,7 +15,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.os.WindowsUtils;
 
 /**
@@ -263,6 +262,7 @@ public class BrowserKeyword extends DriverFactory {
 	 * @author jiaozhongbin
 	 */
 	public static String getBrowserUrl() {
+		
 		return driver.getCurrentUrl();
 	}
 
